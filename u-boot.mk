05268480e9b97f410c1c532928fbe6cf39c6f47a
Wig Cheng <wig.cheng@technexion.com>
2020-10-08 10:10:09 +0800

    u-boot.mk: add axon-imx8mp support

--- u-boot.mk
+++ u-boot.mk
@@ -8,7 +8,6 @@ include common.mk
 UBOOT_BRANCH := tn-imx_v2020.04_5.4.24_2.1.0-next
 UBOOT_COMMIT := `git ls-remote https://github.com/TechNexion/u-boot-tn-imx.git $(UBOOT_BRANCH) | awk '{print $$1}'`
 UBOOT_ARCHIVE := https://github.com/TechNexion/u-boot-tn-imx/archive/$(UBOOT_COMMIT).tar.gz
-UBOOT_DEFCONFIG := pico-imx8mm_defconfig
 
 all: build
 
@@ -21,10 +20,18 @@ distclean: clean
 	rm -rf $(wildcard $(UBOOT_DIR/u-boot-tn-imx))
 
 build: src
-	@sed -i 's/imx8mm-pico-pi.dtb/imx8mm-pico-pi-ili9881c.dtb/' $(UBOOT_DIR)/u-boot-tn-imx/configs/pico-imx8mm_defconfig
+ifeq ($(PLATFORM),pico-imx8mm)
+	$(eval UBOOT_DEFCONFIG := pico-imx8mm_defconfig)
+	$(eval ATF_OPTION := imx8mm-pico-pi)
+	@sed -i 's/imx8mm-pico-pi.dtb/imx8mm-pico-pi-ili9881c.dtb/' $(UBOOT_DIR)/u-boot-tn-imx/configs/pico-imx8mm_defconfig;
+else ifeq ($(PLATFORM),axon-imx8mp)
+	$(eval UBOOT_DEFCONFIG := axon-imx8mp_defconfig)
+	$(eval ATF_OPTION := imx8mp-axon)
+endif
+
 	$(MAKE) ARCH=arm CROSS_COMPILE=${CC} -C $(UBOOT_DIR)/u-boot-tn-imx $(UBOOT_DEFCONFIG)
 	$(MAKE) ARCH=arm CROSS_COMPILE=${CC} -C $(UBOOT_DIR)/u-boot-tn-imx -j$(CPUS) all
-	cd $(UBOOT_DIR)/u-boot-tn-imx; yes | ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- ./install_uboot_imx8.sh -b imx8mm-pico-pi -d /dev/null > /dev/null; cd -
+	cd $(UBOOT_DIR)/u-boot-tn-imx; yes | ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- ./install_uboot_imx8.sh -b $(ATF_OPTION) -d /dev/null > /dev/null; cd -
 
 src:
 	mkdir -p $(UBOOT_DIR)

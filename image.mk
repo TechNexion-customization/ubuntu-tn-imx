9eb2483404a0eb572d35bcf61479844b7f845986
Wig Cheng <wig.cheng@technexion.com>
2020-11-17 10:39:21 +0800

    image.mk: add edm-imx8m to support list

--- image.mk
+++ image.mk
@@ -13,19 +13,21 @@ clean:
 	rm -rf $(OUTPUT_DIR)/$(DEFAULT_IMAGE)
 distclean: clean
 
-build-rootfs:
+build-image:
 ifeq ($(PLATFORM),pico-imx8mm)
 	$(eval TARGET := pico-imx8mm)
 else ifeq ($(PLATFORM),axon-imx8mp)
 	$(eval TARGET := axon-imx8mp)
 else ifeq ($(PLATFORM),edm-g-imx8mp)
-	$(eval TARGET := imx8mp-edm-g)
+	$(eval TARGET := edm-g-imx8mp)
+else ifeq ($(PLATFORM),edm-imx8m)
+	$(eval TARGET := edm-imx8m)
 endif
 
 	@echo "image generating..."
 	./gen_image.sh $(TARGET)
 	@mv test.img $(OUTPUT_DIR)/$(DEFAULT_IMAGE)
 
-build: build-rootfs
+build: build-image
 
-.PHONY: build-rootfs build
+.PHONY: build-image build

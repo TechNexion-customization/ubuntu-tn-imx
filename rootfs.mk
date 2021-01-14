c5a2291efa58b21dfe82995530a49fefa48b9b47
Wig Cheng <wig.cheng@technexion.com>
2021-01-14 14:35:56 +0800

    rootfs.mk: correct axon-e-imx8mp target name

--- rootfs.mk
+++ rootfs.mk
@@ -18,7 +18,7 @@ build-rootfs: src
 ifeq ($(PLATFORM),pico-imx8mm)
 	$(eval TARGET := pico-imx8mm)
 else ifeq ($(PLATFORM),axon-e-imx8mp)
-	$(eval TARGET := axon-imx8mp)
+	$(eval TARGET := axon-e-imx8mp)
 else ifeq ($(PLATFORM),edm-g-imx8mp)
 	$(eval TARGET := edm-g-imx8mp)
 else ifeq ($(PLATFORM),edm-imx8m)

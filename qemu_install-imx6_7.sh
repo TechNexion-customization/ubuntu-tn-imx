0d7b57aa13212295a73aeeb5ae552b8c02020883
Wig Cheng <wig.cheng@technexion.com>
2021-04-13 18:35:05 +0800

    qemu_install-imx6_7: correct update command without sudo permission

--- qemu_install-imx6_7.sh
+++ qemu_install-imx6_7.sh
@@ -27,8 +27,8 @@ deb http://ports.ubuntu.com/ubuntu-ports/ focal-security main
 END
 
 # apt-get source update and installation
-yes "Y" | sudo apt-get update
-yes "Y" | sudo apt-get upgrade
+yes "Y" | apt-get update
+yes "Y" | apt-get upgrade
 yes "Y" | apt install openssh-server iw wpasupplicant hostapd util-linux procps iproute2 haveged dnsmasq iptables net-tools ppp ntp ntpdate bridge-utils can-utils v4l-utils usbutils
 yes "Y" | apt install bash-completion ifupdown resolvconf alsa-utils gpiod cloud-utils udhcpc feh modemmanager software-properties-common bluez blueman
 
@@ -37,7 +37,7 @@ yes "Y" | apt install bash-completion ifupdown resolvconf alsa-utils gpiod cloud
 yes "Y" | apt install gnupg apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu focal stable"
-yes "Y" | sudo apt-get update
+yes "Y" | apt-get update
 apt-cache policy docker-ce
 yes "Y" | apt install docker-ce
 

c7ed5677dfd0e04d4d37af6512fa6c5bfaea8255
Wig Cheng <wig.cheng@technexion.com>
2021-05-25 19:31:08 +0800

    qemu_install: add rng-tools to fix boot haveged service failed issue
    It's a security issue, thanks for Sanjeev<sanjeev.sharma@mikara.co>'s
    feedback.
    
    Test method: cat /proc/sys/kernel/random/entropy_avail
    Pass condition: feedback value > 3000

--- qemu_install.sh
+++ qemu_install.sh
@@ -29,7 +29,7 @@ yes "Y" | sudo apt-get update
 yes "Y" | sudo apt-get upgrade
 yes "Y" | sudo apt --fix-broken install
 yes "Y" | apt install openssh-server iw wpasupplicant hostapd util-linux procps iproute2 haveged dnsmasq iptables net-tools ppp ntp ntpdate bridge-utils can-utils v4l-utils usbutils
-yes "Y" | apt install bash-completion ifupdown resolvconf alsa-utils gpiod cloud-utils libpolkit-agent-1-0 libpolkit-gobject-1-0 policykit-1 udhcpc
+yes "Y" | apt install bash-completion ifupdown resolvconf alsa-utils gpiod cloud-utils libpolkit-agent-1-0 libpolkit-gobject-1-0 policykit-1 udhcpc rng-tools
 
 #install docker-ce
 yes "Y" | apt install gnupg apt-transport-https ca-certificates curl software-properties-common

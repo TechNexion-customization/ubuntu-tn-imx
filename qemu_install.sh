f3ec5550104d076d171ab70fea7d1a9d1b7877a8
Roger, Huang <rogerhuang7@gmail.com>
2022-08-03 08:09:07 +0800

    install: oyside2, and dependence package

--- qemu_install.sh
+++ qemu_install.sh
@@ -76,6 +76,10 @@ yes "Y" | apt install libnss3 libwebpdemux2 libxslt1.1
 # QT5
 yes "Y" | apt install libdouble-conversion3
 
+# pyside2
+yes "Y" | apt install python3-pyside2.qt3dcore python3-pyside2.qt3dinput python3-pyside2.qt3dlogic python3-pyside2.qt3drender python3-pyside2.qtcharts python3-pyside2.qtconcurrent python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qthelp python3-pyside2.qtlocation python3-pyside2.qtmultimedia python3-pyside2.qtmultimediawidgets python3-pyside2.qtnetwork python3-pyside2.qtopengl python3-pyside2.qtpositioning python3-pyside2.qtprintsupport python3-pyside2.qtqml python3-pyside2.qtquick python3-pyside2.qtquickwidgets python3-pyside2.qtscript python3-pyside2.qtscripttools python3-pyside2.qtsensors python3-pyside2.qtsql python3-pyside2.qtsvg python3-pyside2.qttest python3-pyside2.qttexttospeech python3-pyside2.qtuitools python3-pyside2.qtwebchannel python3-pyside2.qtwebsockets python3-pyside2.qtwidgets python3-pyside2.qtx11extras python3-pyside2.qtxml python3-pyside2.qtxmlpatterns
+yes "Y" | apt install python3-pip
+
 #docker part
 groupadd docker
 usermod -aG docker ubuntu
@@ -112,6 +116,14 @@ dpkg -i /opt/deb/5gnr/nm/network-manager_1.26.2-1ubuntu1_arm64.deb
 
 # tn nfc
 dpkg -i /opt/deb/nfc/tn-nfc_1.0.0-0.1_arm64.deb
+
+# osprey packages
+pip3 install pyserial
+pip3 install  ifaddr
+pip3 install paho-mqtt
+pip3 install python-dotenv
+pip3 install requests
+pip3 install zstandard
 
 sudo systemctl daemon-reload
 sudo systemctl disable getty@tty1.service
